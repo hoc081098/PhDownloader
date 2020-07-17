f9f97be8dd852d12ffff0656ff67574d065f72f9
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2020-07-17 21:23:12 +0700

    Initial Commit

---
+++ PhDownloader/PhDownloader.h
@@ -0,0 +1,19 @@
+//
+//  PhDownloader.h
+//  PhDownloader
+//
+//  Created by Petrus on 7/17/20.
+//  Copyright © 2020 Petrus Nguyễn Thái Học. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PhDownloader.
+FOUNDATION_EXPORT double PhDownloaderVersionNumber;
+
+//! Project version string for PhDownloader.
+FOUNDATION_EXPORT const unsigned char PhDownloaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhDownloader/PublicHeader.h>
+
+
